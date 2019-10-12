de2071633284a8efbf707977b03521b3bc58d4e5
bgibson9 <gib15013@byui.edu>
2019-10-12 11:03:50 -0600

    push js

--- lesson04/js/currentdate.js
+++ lesson04/js/currentdate.js
@@ -1,15 +1,19 @@
 var options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long', 
+    year: 'numeric',
 }
 
-var d = new Date();
-var dayName = daynames [d.getDay()];
-var monthName = months[d.getMonth()];
-var fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+//var d = new Date();
+//var dayName = daynames [d.getDay()];
+//var monthName = months[d.getMonth()];
+//var fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
 
-document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
+
+document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-gb', options);
 
 document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
+
+   
+
