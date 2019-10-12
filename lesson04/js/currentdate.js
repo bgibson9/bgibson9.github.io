932453fa1f093f034a767369f27203a0d700799d
bgibson9 <gib15013@byui.edu>
2019-10-12 11:15:43 -0600

    push changes to js

--- lesson04/js/currentdate.js
+++ lesson04/js/currentdate.js
@@ -5,12 +5,6 @@ var options = {
     year: 'numeric',
 }
 
-//var d = new Date();
-//var dayName = daynames [d.getDay()];
-//var monthName = months[d.getMonth()];
-//var fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
-
-
 document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-gb', options);
 
 document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
