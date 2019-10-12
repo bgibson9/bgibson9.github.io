98b71b04f6a4f9072e8dbb5f0833524ca4e46004
bgibson9 <gib15013@byui.edu>
2019-10-12 10:58:36 -0600

    push changes to js dates

--- lesson04/js/currentdate.js
+++ lesson04/js/currentdate.js
@@ -1,8 +1,15 @@
+var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+}
+
 var d = new Date();
 var dayName = daynames [d.getDay()];
 var monthName = months[d.getMonth()];
 var fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
 
-document.getElementById('currentDate').textContent = fulldate;
+document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
 
-document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();''
+document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
