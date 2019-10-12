e10520d8ab77c3917d1634d311a4f35ca89ff055
bgibson9 <gib15013@byui.edu>
2019-10-12 10:15:52 -0600

    fix date

--- lesson04/js/currentdate.js
+++ lesson04/js/currentdate.js
@@ -1,11 +1,8 @@
-
-function getDate() {
 var d = new Date();
 var dayName = daynames [d.getDay()];
 var monthName = months[d.getMonth()];
 var fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
 
-}
-document.getElementById('currentdate').textContent = fulldate;
+document.getElementById('currentDate').textContent = fulldate;
 
 document.getElementById('copyright').textContent = new Date(document.lastModified).getFullYear();
