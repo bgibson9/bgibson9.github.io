1cde83cfc68cc93ab837181a2005a519b1cd64d8
bgibson9 <gib15013@byui.edu>
2019-10-24 17:57:48 -0600

    js

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -8,7 +8,7 @@ function calcWindChill(tempF, speed) {
 	var t = tempF;
     var s = speed;
     //return statement if not within bounds
-    if (t <=50 && s >= 3) {
+    if (t >=50 && s <= 3) { 
         return 0;
     }
     //else calculate and return
@@ -16,6 +16,6 @@ function calcWindChill(tempF, speed) {
         var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16));
         return f;
     }
-
+}
 //Do I need an else?
 //How do I account for tempF <= 50 and 	windSpeed > 3
