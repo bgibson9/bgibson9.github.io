320a7de4cebf4de42e87e055908710874412c8f5
bgibson9 <gib15013@byui.edu>
2019-10-24 16:59:29 -0600

    try more javascript ideas

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -10,8 +10,10 @@ function calcWindChill(tempF, speed) {
 	var s = speed;
 	var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16));
         return f;
-    if t <=50 and s > 3
-    then wChill;
+    if t <=50 and s > 3 {
+        document.getElementsByClassName('wChill')[0].textContent = calcWindChill;
+    }
+//Do I need an else?
 //How do I account for tempF <= 50 and 	windSpeed > 3	
 }
 
