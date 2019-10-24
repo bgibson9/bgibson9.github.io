f3e831749a289aa5e1ce871b246a51b1c498c388
bgibson9 <gib15013@byui.edu>
2019-10-24 17:29:16 -0600

    js after tutor help with code

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -2,20 +2,23 @@
 var tempF = parseFloat(document.getElementsByClassName('temp')[0].textContent);
 var speed = parseFloat(document.getElementsByClassName('windSpeed')[0].textContent);
 var f = calcWindChill(tempF, speed);
-	 document.getElementsByClassName('wChill')[0].textContent = calcWindChill;
+	 document.getElementsByClassName('wChill')[0].textContent - f;
 
 
 function calcWindChill(tempF, speed) {
 	var t = tempF;
-	var s = speed;
+    var s = speed;
+    //return statement if not within bounds
+    if (t <=50 && s >= 3) {
+        return 0;
+    }
+    //else calculate and return
 	var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16));
         return f;
-    if (t <=50 && s > 3) {
-        document.getElementsByClassName('wChill')[0].textContent = calcWindChill;
     }
 
 //Do I need an else?
 //How do I account for tempF <= 50 and 	windSpeed > 3	
-}
 
-         
+
+ //if 2nd line was document.getElementsByClassName('wChill')[0].textContent = calcWindChill;        
