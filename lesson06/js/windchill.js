5cfa8261ac308f12cf3d8881d7d7f68a5bd761b6
bgibson9 <gib15013@byui.edu>
2019-10-25 20:39:35 -0600

    fix .toFixed();

--- lesson06/js/windchill.js
+++ lesson06/js/windchill.js
@@ -2,13 +2,13 @@
 var tempF = parseFloat(document.getElementsByClassName("temp")[0].textContent);
 var speed = parseFloat(document.getElementsByClassName("windSpeed")[0].textContent);
 var f = calcWindChill(tempF, speed);
-	 document.getElementsByClassName("wChill")[0].textContent = f.toFixed();
+	 document.getElementsByClassName("wChill")[0].textContent = f;
 
 function calcWindChill(tempF, speed) {
 	var t = tempF;
     var s = speed;
     //return statement if not within bounds (above 50 and wind speed above 3) no windchill
-    if (t >=50 && s <= 3) { 
+    if (t >=50 || s <= 3) { 
         return "N/A";
 
     }
@@ -16,7 +16,7 @@ function calcWindChill(tempF, speed) {
     //else calculate and return
 	else {
         var f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16));
-        return f;
+        return f.toFixed();
     }
 }
 
