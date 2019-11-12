04fb8d59e255e7a9127b21fb5f6a3f65d3f17a85
bgibson9 <gib15013@byui.edu>
2019-11-12 01:38:15 -0700

    one more try on birthdate

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -17,17 +17,18 @@ fetch(requestURL)
         let h2 = document.createElement("h2");
         //let birthdate = document.createElement("birthdate");
         //let p = document.createElement("birthdate");
-        //let birthdate = document.createElement("p");
+        let birthdate = document.createElement("p");
         //let birthplace = document.createElement("birthplace");
         //let p = document.createElement("birthplace");
         //let img = document.createElement("img");
         //let order = document.createElement("order");
         
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        //p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+        p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
        // p2.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
 
         card.appendChild(h2);
+        card.appendChild(p);
         //card.appendChild(birthdate);
         //card.appendChild(birthplace);
         
