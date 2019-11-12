c6e1026caf62a5bef016f23b569c42ca39228d3f
bgibson9 <gib15013@byui.edu>
2019-11-12 01:21:41 -0700

    uncomment lines

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -16,12 +16,14 @@ fetch(requestURL)
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
         //let birthdate = document.createElement("birthdate");
+        let p = document.createElement("birthdate");
         //let birthplace = document.createElement("birthplace");
+        let p = document.createElement("birthplace");
         //let img = document.createElement("img");
         //let order = document.createElement("order");
         
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        //p1.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+        p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
        // p2.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
 
         card.appendChild(h2);
