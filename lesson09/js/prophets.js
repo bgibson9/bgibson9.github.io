23fe73f1a6924ebfcf0c161883f74dd57c59648a
bgibson9 <gib15013@byui.edu>
2019-11-12 01:08:23 -0700

    fix js error birthplace not defined

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -15,14 +15,14 @@ fetch(requestURL)
 
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
-        let p1 = document.createElement("p");
-        let p2 = document.createElement("p");
+        let birthplace = document.createElement("p1");
+        let birthdate = document.createElement("p2");
         let img = document.createElement("img");
         let order = document.createElement("order");
         
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
         p1.textContent = 'Date of Birth: ' + ' ' + prophets[i].birthdate;
-        p1.textContent = 'Place of Birth: ' + ' ' + prophets[i].birthplace;
+        p2.textContent = 'Place of Birth: ' + ' ' + prophets[i].birthplace;
 
         card.appendChild(h2);
         card.appendChild(birthdate);
