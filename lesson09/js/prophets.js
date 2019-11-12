1322be5bff6f0e99a4eb880196e91a0c31ce3f92
bgibson9 <gib15013@byui.edu>
2019-11-12 01:23:18 -0700

    comment all out again

--- lesson09/js/prophets.js
+++ lesson09/js/prophets.js
@@ -7,7 +7,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const prophets = jsonObject["prophets"];
 
@@ -16,14 +16,14 @@ fetch(requestURL)
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
         //let birthdate = document.createElement("birthdate");
-        let p = document.createElement("birthdate");
+        //let p = document.createElement("birthdate");
         //let birthplace = document.createElement("birthplace");
-        let p = document.createElement("birthplace");
+        //let p = document.createElement("birthplace");
         //let img = document.createElement("img");
         //let order = document.createElement("order");
         
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+        //p.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
        // p2.textContent = 'Place of Birth:' + ' ' + prophets[i].birthplace;
 
         card.appendChild(h2);
